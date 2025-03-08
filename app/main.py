6dca84b61eb6e4490318ccabebb2b15c40eecccf
Max <smolinskijmaksim1@gmail.com>
2025-03-08 11:24:23 +0100

    Fix flake

--- app/main.py
+++ app/main.py
@@ -11,18 +11,18 @@ class Animal:
             Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name},"
-                f"Health: {self.health},"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
 
-    def check_health(self):
-        if self.health <= 0:
+    def check_health(self) -> None:
+        if self.health <= 0 and self in Animal.alive:
             Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
